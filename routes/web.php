06881929cc5450181f15faad462509a55ad1cd7b
wejder <kontakt@op44.pl>
2024-08-07 12:01:52 +0200

    middleware mollie webhook out

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,7 @@
 <?php
 
 use App\Http\Controllers\PaymentGates\Mollie\Webhooks\WebhookController;
-use App\Http\Middleware\PaymentGates\Mollie\MollieWebhookMiddleware;
 use Illuminate\Support\Facades\Route;
 
-Route::post('payment_gates/mollie/webhook', WebhookController::class)->name('payment_gates.mollie.webhook')->middleware(MollieWebhookMiddleware::class);
+Route::post('payment_gates/mollie/webhook', WebhookController::class)->name('payment_gates.mollie.webhook');
 
